6d7bd5a850a57887138b65ff3db4be6ec95d650c
Rashmi <rashminattoji24@gmail.com>
2024-12-02 14:40:50 +0530

    feat : GET all admin

--- frontend/my-app/src/pages/_app.tsx
+++ frontend/my-app/src/pages/_app.tsx
@@ -1,16 +1,13 @@
-import { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '@/store'; // Import your store and persistor
+// src/pages/_app.tsx
+import { Provider } from "react-redux";
+import { store } from "@/store"; // Adjust the path to your store file.
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...pageProps} />
-      </PersistGate>
+      <Component {...pageProps} />
     </Provider>
   );
 }
-
-export default MyApp;
