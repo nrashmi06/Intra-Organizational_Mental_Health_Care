dc4a75c60c851d1d944d1d80b86b23dbc4d07ac8
RAJAY04 <nnm22ad041@nmamit.in>
2024-12-04 16:33:55 +0530

    fix: session notification issues

--- backend/src/main/java/com/dbms/mentalhealth/service/SessionService.java
+++ backend/src/main/java/com/dbms/mentalhealth/service/SessionService.java
@@ -3,11 +3,12 @@ package com.dbms.mentalhealth.service;
 import com.dbms.mentalhealth.dto.chatMessage.ChatMessageDTO;
 import com.dbms.mentalhealth.dto.session.SessionResponseDTO;
 import com.dbms.mentalhealth.dto.session.SessionSummaryDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.util.List;
 
 public interface SessionService {
-    String initiateSession(Integer listenerId, String message);
+    String initiateSession(Integer listenerId, String message) throws JsonProcessingException;
     String updateSessionStatus(Integer userId, String action);
     SessionResponseDTO getSessionById(Integer sessionId);
     List<SessionSummaryDTO> getAllSessions();
