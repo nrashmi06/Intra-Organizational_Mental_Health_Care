bf6294c663baceeb5ca54a70f18bb14e3ea86e57
RAJAY04 <nnm22ad041@nmamit.in>
2024-12-24 12:45:59 +0530

    feat: cachable adminServiceImpl

--- backend/src/main/java/com/dbms/mentalhealth/config/CacheConfig.java
+++ backend/src/main/java/com/dbms/mentalhealth/config/CacheConfig.java
@@ -2,9 +2,13 @@ package com.dbms.mentalhealth.config;
 
 import com.dbms.mentalhealth.dto.blog.response.BlogResponseDTO;
 import com.dbms.mentalhealth.dto.blog.response.BlogSummaryDTO;
+import com.dbms.mentalhealth.dto.Admin.response.AdminProfileResponseDTO;
+import com.dbms.mentalhealth.dto.Admin.response.AdminProfileSummaryResponseDTO;
+import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +21,13 @@ import java.util.concurrent.TimeUnit;
 public class CacheConfig {
     private static final Logger logger = LoggerFactory.getLogger(CacheConfig.class);
 
+    @Value("${cache.duration.minutes}")
+    private Long cacheExpiry;
+
     @Bean
-    public com.github.benmanes.caffeine.cache.Cache<Integer, BlogResponseDTO> blogCache() {
+    public Cache<Integer, BlogResponseDTO> blogCache() {
         return Caffeine.newBuilder()
-                .expireAfterAccess(1, TimeUnit.MINUTES)  // Expire after 1 minute of inactivity
+                .expireAfterAccess(cacheExpiry, TimeUnit.MINUTES)
                 .maximumSize(100)
                 .removalListener((key, value, cause) ->
                         logger.info(String.format("Key %s was removed (%s)", key, cause)))
@@ -29,13 +36,35 @@ public class CacheConfig {
     }
 
     @Bean
-    public com.github.benmanes.caffeine.cache.Cache<String, List<BlogSummaryDTO>> blogListCache() {
+    public Cache<String, List<BlogSummaryDTO>> blogListCache() {
         return Caffeine.newBuilder()
-                .expireAfterWrite(1, TimeUnit.MINUTES)
+                .expireAfterWrite(cacheExpiry, TimeUnit.MINUTES)
                 .maximumSize(50)
                 .removalListener((key, value, cause) ->
                         logger.info(String.format("Key %s was removed (%s)", key, cause)))
-                .recordStats() // Enable statistics recording
+                .recordStats()
+                .build();
+    }
+
+    @Bean
+    public Cache<Integer, AdminProfileResponseDTO> adminCache() {
+        return Caffeine.newBuilder()
+                .expireAfterAccess(cacheExpiry, TimeUnit.MINUTES)
+                .maximumSize(100)
+                .removalListener((key, value, cause) ->
+                        logger.info(String.format("Key %s was removed (%s)", key, cause)))
+                .recordStats()
+                .build();
+    }
+
+    @Bean
+    public Cache<String, List<AdminProfileSummaryResponseDTO>> adminListCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(cacheExpiry, TimeUnit.MINUTES)
+                .maximumSize(50)
+                .removalListener((key, value, cause) ->
+                        logger.info(String.format("Key %s was removed (%s)", key, cause)))
+                .recordStats()
                 .build();
     }
 }
--- backend/src/main/java/com/dbms/mentalhealth/scheduler/CacheScheduler.java
+++ backend/src/main/java/com/dbms/mentalhealth/scheduler/CacheScheduler.java
@@ -1,5 +1,6 @@
 package com.dbms.mentalhealth.scheduler;
 
+import com.dbms.mentalhealth.service.cachableImpl.CacheableAdminServiceImpl;
 import com.dbms.mentalhealth.service.cachableImpl.CacheableBlogServiceImpl;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,14 +8,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class CacheScheduler {
 
-    private final CacheableBlogServiceImpl cacheableBlogService;
+    private final CacheableBlogServiceImpl cacheableBlogServiceImpl;
+    private final CacheableAdminServiceImpl cacheableAdminServiceImpl;
 
-    public CacheScheduler(CacheableBlogServiceImpl cacheableBlogService) {
-        this.cacheableBlogService = cacheableBlogService;
+    public CacheScheduler(CacheableBlogServiceImpl cacheableBlogService, CacheableAdminServiceImpl cacheableAdminServiceImpl) {
+        this.cacheableBlogServiceImpl = cacheableBlogService;
+        this.cacheableAdminServiceImpl = cacheableAdminServiceImpl;
     }
 
     @Scheduled(fixedRateString = "${scheduler.user-activity-cleanup-interval}")
     public void clearCache() {
-        cacheableBlogService.logCacheStats();
+        cacheableBlogServiceImpl.logCacheStats();
+        cacheableAdminServiceImpl.logCacheStats();
     }
 }
