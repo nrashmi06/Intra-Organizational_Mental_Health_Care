092c6c749a839951df2c160f88e60d2e2779f1f2
Bhavya Nayak <nnm22cs040@nmamit.in>
2024-12-25 10:58:09 +0530

    fix: loader for small screens

--- frontend/my-app/src/components/ui/loading.tsx
+++ frontend/my-app/src/components/ui/loading.tsx
@@ -2,12 +2,14 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Loading = () => {
   return (
-    <div className="w-full h-full fixed">
-      <DotLottieReact
-        src="https://lottie.host/715e63f9-ea4c-41ec-aad4-b6b3fcdf5c0a/6WcEMMWJBD.lottie"
-        loop
-        autoplay
-      />
+    <div className="fixed inset-0 w-screen h-screen flex items-center justify-center bg-white/80">
+      <div className="w-full max-w-md aspect-square">
+        <DotLottieReact
+          src="https://lottie.host/715e63f9-ea4c-41ec-aad4-b6b3fcdf5c0a/6WcEMMWJBD.lottie"
+          loop
+          autoplay
+        />
+      </div>
     </div>
   );
 };
