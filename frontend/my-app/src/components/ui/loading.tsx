7be7423947981d3791dd1120e4e2cefed539b928
Bhavya Nayak <nnm22cs040@nmamit.in>
2024-12-25 19:43:26 +0530

    fix: added regex to email

--- frontend/my-app/src/components/ui/loading.tsx
+++ frontend/my-app/src/components/ui/loading.tsx
@@ -2,14 +2,12 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 w-screen h-screen flex items-center justify-center bg-white/80">
-      <div className="w-full max-w-md aspect-square">
-        <DotLottieReact
-          src="https://lottie.host/715e63f9-ea4c-41ec-aad4-b6b3fcdf5c0a/6WcEMMWJBD.lottie"
-          loop
-          autoplay
-        />
-      </div>
+    <div className="w-full max-w-72 aspect-square">
+      <DotLottieReact
+        src="https://lottie.host/715e63f9-ea4c-41ec-aad4-b6b3fcdf5c0a/6WcEMMWJBD.lottie"
+        loop
+        autoplay
+      />
     </div>
   );
 };
