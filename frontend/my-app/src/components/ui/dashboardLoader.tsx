ecced762948874810e2204efadaad81b255ec964
Rashmi <rashminattoji24@gmail.com>
2025-01-04 04:11:26 +0530

    fix : build error fix - 3

--- frontend/my-app/src/components/ui/dashboardLoader.tsx
+++ frontend/my-app/src/components/ui/dashboardLoader.tsx
@@ -1,8 +1,6 @@
-import "@/styles/global.css";
 const DashboardLoader = () => {
   return (
     <>
-      {/* Full screen overlay with high z-index */}
       <div className="fixed inset-0 z-[9999] bg-white/80 md:ml-64">
         <div className="flex h-full w-full items-center justify-center">
           <div className="loader"></div>
