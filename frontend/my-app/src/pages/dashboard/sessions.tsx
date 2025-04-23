5be599979b345a846265f2fd801a96cca46163a4
Rashmi <rashminattoji24@gmail.com>
2025-04-23 14:07:02 +0530

    update : session case study page

--- frontend/my-app/src/pages/dashboard/sessions.tsx
+++ frontend/my-app/src/pages/dashboard/sessions.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { LiveSessions } from "@/components/dashboard/session/Live";
 import { CompletedSessions } from "@/components/dashboard/session/Completed";
 import DashboardLayout from "@/components/dashboard/DashboardLayout";
+import { SessionCardList } from "@/components/dashboard/session/SessionCardList";
 
 const SessionsPage = () => {
   return (
@@ -12,12 +13,16 @@ const SessionsPage = () => {
         <TabsList>
           <TabsTrigger value="online">Live Sessions</TabsTrigger>
           <TabsTrigger value="registered">All Sessions</TabsTrigger>
+          <TabsTrigger value="summary">Case Study</TabsTrigger>
         </TabsList>
         <TabsContent value="online" className="space-y-4">
           <LiveSessions />
         </TabsContent>
         <TabsContent value="registered" className="space-y-4">
           <CompletedSessions />
+        </TabsContent>
+        <TabsContent value="summary" className="space-y-4">
+          < SessionCardList  />
         </TabsContent>
       </Tabs>
     </div>
